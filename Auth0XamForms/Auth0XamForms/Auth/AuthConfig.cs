d82923e6f1977edc0c31adf4f7cddd710ff9af04
ECR\EgonRasmussen <ecr@eucsyd.dk>
2022-05-13 13:24:55 +0200

    ok

--- Auth0XamForms/Auth0XamForms/Auth/AuthConfig.cs
+++ Auth0XamForms/Auth0XamForms/Auth/AuthConfig.cs
@@ -2,8 +2,8 @@
 {
     public static class AuthConfig
     {
-        public const string Domain = "";    // Her indsættes Domain Name fra Auth0
-        public const string ClientId = "";  // Her indsættes Client ID fra Auth0
-        public const string Audience = "";  // Her indsættes Audience fra Auth0
+        public const string Domain = "eucsyd.eu.auth0.com";    // Her indsættes Domain Name fra Auth0
+        public const string ClientId = "SamcgMljPCrHhr0sFIwj5w8zC7Xs8I69";  // Her indsættes Client ID fra Auth0
+        public const string Audience = "https://localhost:5000/weatherforecast";  // Her indsættes Audience fra Auth0
     }
 }
