63fb1d3539f50951e3f44f10875d902adb02e281
Clone Egon C. Rasmussen <ecr@eucsyd.dk>
2022-05-17 10:53:36 +0200

    ok

--- Auth0XamForms/Auth0XamForms/AuthConfig.cs
+++ Auth0XamForms/Auth0XamForms/AuthConfig.cs
@@ -2,8 +2,8 @@
 
 public static class AuthConfig
 {
-    public const string Domain = "";    // Her indsættes Domain name fra Auth0
-    public const string ClientId = "";  // Her indsættes Client ID fra Auth0 (Application)
+    public const string Domain = "eucsyd.eu.auth0.com";    // Her indsættes Domain name fra Auth0
+    public const string ClientId = "SamcgMljPCrHhr0sFIwj5w8zC7Xs8I69";  // Her indsættes Client ID fra Auth0 (Application)
     public const string Audience = "https://localhost:5000/weatherforecast";  // Her indsættes Audience fra Auth0 (API)
     public const string Scopes = "openid profile read:weatherforecast"; // Her indsættes de ekstra scopes fra Permissions fra Auth0 (API)
     public const string PackageName = "com.companyname.auth0xamforms";  // Her indsættes <package> fra AndroidManifestet
